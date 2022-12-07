da7e8d7403ac0eb777f2f30d95e0bd3d3df2f84c
Maneesha Kumari <manithakur1224@gmail.com>
2022-12-07 07:35:07 +0530

    created pallindrome and prime file

---
+++ pallindrome.js
@@ -0,0 +1,13 @@
+let string="Naman"
+function reverse (string){
+    let bag="";
+    for(let i=string.length-1;i>=0;i--){
+        bag +=string[i];
+    }return bag;
+}
+let reversed=reverse(string);
+if(string==reversed){
+    console.log("Palindrome");
+}else{
+    console.log("NA")
+}
